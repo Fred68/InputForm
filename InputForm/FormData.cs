d623c72ccc5b95dfb03001f22d2e593042fe44ea
Federico Cerutti <fred68to@hotmail.it>
2024-12-30 23:36:05 +0100

    Creata nuova classe FormData. InputInfo sottclasse.

---
+++ InputForm/FormData.cs
@@ -0,0 +1,209 @@
+﻿using Fred68.InputForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Dynamic;
+using System.Collections;
+
+namespace InputForms
+{
+	public class FormData
+	{
+		List<InputForm.InputInfo> data;
+
+		/// <summary>
+		/// Is name (key) free ?
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		bool isFree(string name)
+		{
+			bool found = false;
+			foreach(InputForm.InputInfo info in data)
+			{
+				if(info.Name == name)
+				{
+					found = true;
+					break;
+				}
+			}
+			return !found;
+		}
+
+		public int Count
+		{
+			get { return data.Count; }
+		}
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		public FormData()
+		{
+			data = new List<InputForm.InputInfo>();
+		}
+
+		/// <summary>
+		/// Add integer
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, int x, bool read_only = false, bool dropdown = false)
+		{
+			bool ret = isFree(name);
+			if(ret)	data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+		/// <summary>
+		/// Add boolean
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, bool x, bool read_only = false, bool dropdown = false)
+		{
+			bool ret = isFree(name);
+			data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+		/// <summary>
+		/// Add string
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, string x, bool read_only = false, bool dropdown = false)
+		{
+			bool ret = isFree(name);
+			data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+		/// <summary>
+		/// Add float
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, float x, bool read_only = false, bool dropdown = false)
+		{
+			bool ret = isFree(name);
+			data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+		/// <summary>
+		/// Add double
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, double x, bool read_only = false, bool dropdown = false)
+		{	
+			bool ret = isFree(name);
+			data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+		/// <summary>
+		/// Add DateTime
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="x"></param>
+		/// <param name="read_only"></param>
+		/// <param name="dropdown"></param>
+		/// <returns></returns>
+		public bool Add(string name, DateTime x, bool read_only = false, bool dropdown = false)
+		{
+			bool ret = isFree(name);
+			data.Add(new InputForm.InputInfo(name, x, read_only, dropdown));
+			return ret;
+		}
+
+		/// <summary>
+		/// Get value from name (key)
+		/// </summary>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		/// <exception cref="KeyNotFoundException"></exception>
+		public dynamic this[string key]
+		{
+			get
+			{
+				foreach(InputForm.InputInfo info in data)
+				{
+					if(info.Name == key)
+					{
+						return (dynamic)info.Dt.Get();
+					}
+				}
+				throw new KeyNotFoundException();		
+			}
+		}
+		/// <summary>
+		/// Name (key) esists 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public bool Contains(string name)
+		{
+			return !isFree(name);
+		}
+
+		/// <summary>
+		/// Names enumeration
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<string> Names()
+		{
+			foreach(InputForm.InputInfo info in data)
+			{
+				yield return info.Name;
+			}
+			yield break;
+		}
+
+		public IEnumerable<InputForm.InputInfo> Info()
+		{
+			foreach(InputForm.InputInfo info in data)
+			{
+				yield return info;
+			}
+			yield break;
+		}
+		/// <summary>
+		/// Dump all values into string
+		/// </summary>
+		/// <returns></returns>
+
+		public InputForm.InputInfo Info(int index)
+		{
+			return data[index];
+		}
+
+		public string Dump(bool onlyModified = false)
+		{
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<data.Count; i++)
+			{
+				if(!onlyModified || data[i].isModified)
+				{
+					sb.AppendLine($"{data[i].Name} : {data[i].Dt.Get().ToString()}");	
+				}
+			}
+			return sb.ToString();
+		}
+		
+	}
+}
--- InputForm/InputForm.cs
+++ InputForm/InputForm.cs
@@ -1,23 +1,95 @@
 ﻿
 using Fred68.GenDictionary;
-using Fred68.InputForm;
-using Microsoft.VisualBasic;
-using Microsoft.Win32;
-using System.CodeDom;
+using InputForms;
 using System.Text;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
-namespace Fred68.InputForm
+namespace Fred68.InputForms
 {
 	public class InputForm:Form
 	{
 		static string tbPrefixName = "_tbValue";
-		List<InputInfo> _datList;
+		
+		public class InputInfo
+		{
+			string	_name;
+			bool	_readonly;
+			bool	_dropdown;		// Valore Dat string, ma on click apre nuova dialog con la lista
+			Dat		_dat;
+			bool	_isSet;
+			bool	_isModified;
+
+			public string Name {get {return _name;}}
+			public bool isReadonly {get {return _readonly;}}
+			public bool isDropdown {get {return _dropdown;}}
+			public bool isModified {get {return _isModified;} set {_isModified = value;}}
+
+			/// <summary>
+			/// Set and access to data with:
+			/// dynamic Get()
+			/// new Dat(...)
+			/// static Type GetEqType(dynamic x)
+			/// </summary>
+			public Dat Dt {
+							get {
+								if(_isSet)
+									return _dat;
+								else
+									throw new Exception("Not initialized value");
+								}
+							set {
+								_dat = value;
+								_isSet = true;
+								}
+							}
+
+			void _Set(string name, bool read_only = false, bool dropdown = false)
+			{
+				_name = name;
+				_readonly = read_only;
+				_dropdown = dropdown;
+				_isSet = false;
+			}
+
+			public InputInfo(string name, int x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+			public InputInfo(string name, bool x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+			public InputInfo(string name, string x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+			public InputInfo(string name, float x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+			public InputInfo(string name, double x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+			public InputInfo(string name, DateTime x, bool read_only = false, bool dropdown = false)
+			{
+				_Set(name, read_only, dropdown);
+				Dt = new Dat(x);
+			}
+
+
+		}
+
+		FormData _fd;
 		int _maxTxtLength;
 		Label[] _lblNames;
 		Label[] _lblTypes;
-		//Control[] _tbValues;
 		List<Control> _tbValues;
 		bool _isOk;
 
@@ -80,14 +152,14 @@ namespace Fred68.InputForm
 			PerformLayout();
 		}
 
-		public InputForm(List<InputInfo> datList,int maxTxtLength = 20)
+		public InputForm(FormData fd,int maxTxtLength = 20)
 		{
 			_isOk = true;
-			_datList = datList;
+			_fd = fd;
 			_maxTxtLength = maxTxtLength;
 
 			StringBuilder sb = new StringBuilder();
-			foreach(InputInfo info in _datList)
+			foreach(InputForm.InputInfo info in _fd.Info())
 			{
 				sb.AppendLine($"{info.Name}:{info.Dt.Get()}");
 			}
@@ -95,7 +167,6 @@ namespace Fred68.InputForm
 			InitializeComponent();
 			SetupForm();
 		}
-
 
 		void SetupForm()
 		{
@@ -113,46 +184,45 @@ namespace Fred68.InputForm
 
 			_yStep = int.Max(label1.Height,textBox1.Height);
 
-			_lblNames = new Label[_datList.Count];
-			_lblTypes = new Label[_datList.Count];
-			//_tbValues = new TextBox[_datList.Count];
-			_tbValues = new List<Control>(_datList.Count);
+			_lblNames = new Label[_fd.Count];
+			_lblTypes = new Label[_fd.Count];
+			_tbValues = new List<Control>(_fd.Count);
 
 			int lblxmax, tbxmax, lbvmax;
 			lblxmax = tbxmax = lbvmax = -1;
 
 			SuspendLayout();
-			for(int i = 0;i < _datList.Count;i++)
+			for(int i = 0;i < _fd.Count;i++)
 			{
 				_lblNames[i] = new Label();
 				_lblNames[i].Location = new Point(_xNames,_yName + i * _yStep);
 				_lblNames[i].AutoSize = true;
-				_lblNames[i].Text = _datList[i].Name;
-				//_lblNames[i].Name = $"Name{i.ToString()}";
+				_lblNames[i].Text = _fd.Info(i).Name;
 				this.Controls.Add(_lblNames[i]);
 				if(_lblNames[i].Right > lblxmax)
 					lblxmax = _lblNames[i].Right;
 			}
 
-			for(int i = 0;i < _datList.Count;i++)
+			for(int i = 0;i < _fd.Count;i++)
 			{
-				bool isbool = _datList[i].Dt.Get().GetType().ToString() == "System.Boolean";
+				bool isbool = _fd.Info(i).Dt.Get().GetType().ToString() == "System.Boolean";
 				if(!isbool)
 				{
 					_tbValues.Add(new TextBox());
-					_tbValues[i].Text = (_datList[i].Dt.Get()).ToString();
+					((TextBox)_tbValues[i]).Text = (_fd.Info(i).Dt.Get()).ToString();
 				}
 				else
 				{
 					_tbValues.Add(new CheckBox());
+					((CheckBox)_tbValues[i]).Checked = _fd.Info(i).Dt.Get();
 				}
 				_tbValues[i].Location = new Point(lblxmax + _xSpace,_yValue + i * _yStep);
 				_tbValues[i].Name = $"{tbPrefixName}{i.ToString()}";
 				if(!isbool)
 					((TextBox)_tbValues[i]).TextAlign = HorizontalAlignment.Left;
 				_tbValues[i].Size = new Size(txtbWidt,int.Max(label1.Height,textBox1.Height));
-				_tbValues[i].Enabled = !_datList[i].isReadonly;		// Usa Enabled, valido per tutti i controlli, invece che Readonly
-				if(!_datList[i].isReadonly)
+				_tbValues[i].Enabled = !_fd.Info(i).isReadonly;		// Usa Enabled, valido per tutti i controlli, invece che Readonly
+				if(!_fd.Info(i).isReadonly)
 				{
 					if(!isbool)
 						((TextBox)_tbValues[i]).TextChanged += txtChanged_Handler;
@@ -163,12 +233,12 @@ namespace Fred68.InputForm
 				if(_tbValues[i].Right > tbxmax)
 					tbxmax = _tbValues[i].Right;
 			}
-			for(int i = 0;i < _datList.Count;i++)
+			for(int i = 0;i < _fd.Count;i++)
 			{
 				_lblTypes[i] = new Label();
 				_lblTypes[i].Location = new Point(tbxmax + _xSpace,_yName + i * _yStep);
 				_lblTypes[i].AutoSize = true;
-				string txt = (_datList[i].Dt.Get().GetType()).ToString();
+				string txt = (_fd.Info(i).Dt.Get().GetType()).ToString();
 				_lblTypes[i].Text = txt.Substring(txt.LastIndexOf('.') + 1);
 				if(_tbValues[i] is TextBox)
 				{
@@ -181,7 +251,7 @@ namespace Fred68.InputForm
 			this.Controls.Remove(label1);                   // Elimina label e textbox
 			this.Controls.Remove(textBox1);
 
-			btOK.Location = new Point(tbxmax - btOK.Width,_yValue + _datList.Count * _yStep + _ySpace);
+			btOK.Location = new Point(tbxmax - btOK.Width,_yValue + _fd.Count * _yStep + _ySpace);
 			btOK.Click += btOK_Click;
 			btCancel.Location = btOK.Location - new Size(btCancel.Width + _xSpace,0);
 
@@ -214,7 +284,6 @@ namespace Fred68.InputForm
 			ResumeLayout(true);
 
 		}
-
 		private Button btOK;
 		private Label label1;
 		private TextBox textBox1;
@@ -226,7 +295,7 @@ namespace Fred68.InputForm
 			string sname = ((Control)sender).Name;
 			if(int.TryParse(sname.Substring(InputForm.tbPrefixName.Length),out i))
 			{
-				_datList[i].isModified = true;
+				_fd.Info(i).isModified = true;
 			}
 			else
 			{
@@ -237,11 +306,11 @@ namespace Fred68.InputForm
 
 		private void btOK_Click(object sender,EventArgs e)
 		{
-			for(int i = 0;i < _datList.Count;i++)
+			for(int i = 0;i < _fd.Count;i++)
 			{
-				if(_datList[i].isModified)
+				if(_fd.Info(i).isModified)
 				{
-					string t = _datList[i].Dt.Get().GetType().ToString();
+					string t = _fd.Info(i).Dt.Get().GetType().ToString();
 
 					switch(t)
 					{
@@ -250,13 +319,13 @@ namespace Fred68.InputForm
 							int x;
 							if(int.TryParse(_tbValues[i].Text,out x))
 							{
-								_datList[i].Dt = new Dat(x);
+								_fd.Info(i).Dt = new Dat(x);
 							}
 						}
 						break;
 						case "System.String":
 						{
-							_datList[i].Dt = new Dat(_tbValues[i].Text);
+							_fd.Info(i).Dt = new Dat(_tbValues[i].Text);
 						}
 						break;
 						case "System.DateTime":
@@ -264,7 +333,7 @@ namespace Fred68.InputForm
 							DateTime x;
 							if(DateTime.TryParse(_tbValues[i].Text,out x))
 							{
-								_datList[i].Dt = new Dat(x);
+								_fd.Info(i).Dt = new Dat(x);
 							}
 						}
 						break;
@@ -273,7 +342,7 @@ namespace Fred68.InputForm
 							float x;
 							if(float.TryParse(_tbValues[i].Text,out x))
 							{
-								_datList[i].Dt = new Dat(x);
+								_fd.Info(i).Dt = new Dat(x);
 							}
 						}
 						break;
@@ -282,7 +351,7 @@ namespace Fred68.InputForm
 							double x;
 							if(double.TryParse(_tbValues[i].Text,out x))
 							{
-								_datList[i].Dt = new Dat(x);
+								_fd.Info(i).Dt = new Dat(x);
 							}
 						}
 						break;
@@ -290,6 +359,7 @@ namespace Fred68.InputForm
 						{
 							bool x;
 							x = ((CheckBox)_tbValues[i]).Checked;
+							_fd.Info(i).Dt = new Dat(x);
 						}
 						break;
 						default:
